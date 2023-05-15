adb44b41baeef78fbc782c8f0b6426e8d13df162
Rukkyaa <lbrt.axel@gmail.com>
2023-05-15 12:04:46 +0200

    Mandatory done

--- Makefile
+++ Makefile
@@ -13,8 +13,6 @@ up:
 clean:
 	@printf "\033[32m[+] Stopping containers...\033[0m\n"
 	@docker-compose -f $(DOCKER_COMPOSE_FILE) down -v
-	@sudo rm -rf /home/rukkyaa/data/mysql
-	@sudo rm -rf /home/rukkyaa/data/wordpress
 	@printf "\033[32m[+] Containers stopped.\033[0m\n"
 	@printf "\033[32m[+] Removing images...\033[0m\n"
 	@docker rmi -f $$(docker images -q)
--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -2,12 +2,6 @@
 
 # Check if WordPress is already installed
 if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
-
-	# Download WordPress
-	wget https://fr.wordpress.org/wordpress-6.2-fr_FR.tar.gz -P /var/www/html/ && \
-	cd /var/www/html && tar -xvf wordpress-6.2-fr_FR.tar.gz && rm wordpress-6.2-fr_FR.tar.gz && \
-	chown -R root:root /var/www/html/wordpress
-    
 	# Create the wp-config.php file
 	wp config create --allow-root --dbname="$DB_NAME" --dbuser="$DB_USERNAME" --dbpass="$DB_PASSWORD" \
         --dbhost="mariadb:3306" --path="/var/www/html/wordpress"
